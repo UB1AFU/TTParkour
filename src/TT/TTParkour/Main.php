5130a7b117cdf578df9f71356da35c4a2b84ee53
Alianated <seanmcpex@gmail.com>
2015-10-18 08:11:57 -0400

    Update Main.php

--- src/TT/TTParkour/Main.php
+++ src/TT/TTParkour/Main.php
@@ -1,1 +1,30 @@
+<?php
 
+namespace TT\TTParkour;
+
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\math\Vector3;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener{
+
+     public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("TTParkour enabled!");
+     }
+
+     public function onDisable(){
+        $this->getLogger()->info("TTParkour disabled!");
+     }
+   
+     public function onPlayerMove(PlayerMoveEvent $event){
+         $player = $event->getPlayer();
+         $floor = new Vector3($player->getFloorY() - 1);
+          if(($floor == 8)){
+             $player->teleport(118.50, 15, 126.50);
+          }
+     }
+}
